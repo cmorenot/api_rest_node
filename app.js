ce4bbd9693fb9a17021bd4daa34f97c04971480f
cemt <cmorenot@uho.edu.cu>
2023-11-16 22:47:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv").config()
+const express = require("express");
+const cors = require("cors")
+const dbConect =require("./config/mongo")
+const app = express()
+
+app.use(cors())
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log('http://localhost:',port)
+})
+
+dbConect()
